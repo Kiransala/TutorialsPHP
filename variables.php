994545ed9ebeb98ac4bca5a49e263da56048c1c3
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2023-01-04 22:30:48 +0530

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+// in php we dont have to specify the data types 
+$str = "Hello & Welcome"; //str is the variable name
+$num = 5; //num is the variable name
+$float = 7.6; //float is the variable name
+
+echo "<h1>" .$str. "<h1>";
+echo "<h2>" .$num. "<h2>";
+echo "<h3>" .$float. "<h3>";
+
+?>

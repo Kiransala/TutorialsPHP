436be5b6e5c9f17cc125c06515113a1e01a1abfd
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2023-01-05 20:10:41 +0530

    Update variables.php

--- variables.php
+++ variables.php
@@ -1,4 +1,5 @@
 <?php
+
 // in php we dont have to specify the data types 
 $str = "Hello & Welcome"; //str is the variable name
 $num = 5; //num is the variable name
@@ -8,4 +9,9 @@ echo "<h1>" .$str. "<h1>";
 echo "<h2>" .$num. "<h2>";
 echo "<h3>" .$float. "<h3>";
 
+$name = "kiran";
+$age = 19;
+
+echo "My name is $name and my age is $age."
+  
 ?>

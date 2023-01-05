35e660312f495fa9da67f415950768e497be129e
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2023-01-05 20:38:45 +0530

    Create switch.php

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Billy";
+
+switch ($name)
+{
+  case "Alex":
+    echo "You have Blue eyes.";
+    break;
+
+  case "Billy":
+    echo "You have Brown eyes.";
+    break;
+
+  default :
+  echo "I don't know which color your eyes are.";
+}
+
+?>

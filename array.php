aebf95441a893b1ae370a4fef1fc1bffd9642308
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2023-01-05 21:47:06 +0530

    Create array.php

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+
+$days = array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"); //Normal array
+
+// echo "Today is " .$days[3]; //Today is Thursday
+//echo "Today is $days[3]."; // Today is Thursday.
+
+$ages = array("Alex"=>19,"Billy"=>16,"Karl"=>20); //dictionary
+
+//echo $ages['Billy']; //16
+
+$names = array(1=>"Alex",2=>"Billy",3=>"Karl"); 
+
+echo $names[1]; //Alex
+
+?>

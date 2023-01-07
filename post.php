bc94111c95ba874648c123180baca2b0cd371266
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2023-01-07 17:21:17 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,18 +1,18 @@
 <html>
-
 <body>
-  <form action='post.php' method='POST'>
+  <form action='tutorial.php' method='POST'>
+    <h3>Enter password:</h3>
     <input type='password' name='password'><br>
     <input type='submit' value='Click here'>
   </form>
 </body>
-
 </html>
 
 <?php
 
-$password = $_POST['password'];
+@$password = $_POST['password'];
 
 if ($password)
   echo "<h2>Thanks for your password - $password</h2>";
+  
 ?>

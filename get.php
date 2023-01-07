f39674a5cab239c2ad530373188344dbcfe15443
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2023-01-07 17:23:01 +0530

    Update get.php

--- get.php
+++ get.php
@@ -1,19 +1,18 @@
 <html>
-<head>
-  <title>PHP Form</title>
-</head>
+
 <body>
-  <h1>Enter your name:</h1>
   <form action='get.php' method='GET'>
+    <h3>Enter your name:</h3>
     <input type='text' name='myname'><br>
     <input type='submit' value='Click here'>
   </form>
-  <br>
-  <?php
-  if (isset($_GET['myname'])) {
-    $name = $_GET['myname'];
-    echo "<h2>Hello, $name.</h2>";
-  }
-  ?>
 </body>
 </html>
+
+<?php
+
+@$name = $_GET['myname'];
+
+if ($name)
+  echo "<h2>Hello, $name.</h2>";
+?>
